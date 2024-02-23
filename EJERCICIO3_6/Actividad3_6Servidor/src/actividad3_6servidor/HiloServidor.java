ca6a9978b254cf3ce3253bdbe4723a38e97a6d36
Elías Santiago Viera Fernández <a22eliassvf@ATEIS.LOCAL>
2024-02-23 14:19:00 +0100

    ..

---
+++ EJERCICIO3_6/Actividad3_6Servidor/src/actividad3_6servidor/HiloServidor.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad3_6servidor;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author user
+ */
+public class HiloServidor extends Thread {
+    BufferedReader fentrada;
+    PrintWriter fsalida;
+    Socket socket=null;
+                   //constructor del hilo, recibe el socket cliente desde hilo primario servidor
+                   // recibe el socket y  el hilo se encarga ahora de crear flujos de entrada y salida con el cliente
+	long tiempoInicio; // variable para almacenar el tiempo de inicio
+	String direccionIP = "192.168.4.11";
+    
+    public HiloServidor(Socket s) throws IOException {// CONSTRUCTOR
+		socket = s;
+		//el hilo recibe el socket conectado al cliente
+		// el hilo se encarga de crear flujos de entrada y salida para el socket
+		fsalida = new PrintWriter(socket.getOutputStream(), true);
+		fentrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		tiempoInicio = System.currentTimeMillis(); // registrar el tiempo de inicio
+	}
+    
+    public void run() {// tarea a realizar con el cliente, tarea del hilo
+		String cadena = null;
+
+		System.out.println("=> Conectando a la IP " + direccionIP + ", Puerto remoto: " + socket.getPort() + socket.toString());
+        try {
+		cadena = fentrada.readLine();
+	} catch (IOException e) {
+				// TODO Auto-generated catch block
+	   e.printStackTrace();
+	} // obtener  1º cadena del cliente
+	while (!cadena.trim().equals("*")) {
+
+	 fsalida.println(cadena.trim().toUpperCase());// enviar mayúscula
+	try {
+		cadena = fentrada.readLine();
+	 } catch (IOException e) {
+		// TODO Auto-generated catch block
+	  e.printStackTrace();
+	  } // obtener  siguiente cadena del cliente
+	} // fin while
+
+	System.out.println("FIN CON: " + socket.toString());
+	long tiempoFin = System.currentTimeMillis(); // registrar el tiempo de finalización
+	long tiempoTotal = tiempoFin - tiempoInicio; // calcular el tiempo total
+
+	System.out.println("Tiempo total de ejecución para " + socket.toString() + ": " + tiempoTotal + " milisegundos");
+       
+		 
+		
+		 
+		try {
+                        fsalida.close();
+			fentrada.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+    
+}
